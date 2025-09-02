7516d2976a72727dbec61fcbbb45e1b304b5051b
Sri Balaji M <132934551+sribalaji07@users.noreply.github.com>
2025-09-02 01:15:21 -0400

    update static files

--- schemabridge/urls.py
+++ schemabridge/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path('', include('schemabridge.site.urls')),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
